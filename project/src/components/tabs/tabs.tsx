ec5d6a2edebad468fd89d80b9b5b6c81e2260fc9
d.stukov <d.stukov@skbkontur.ru>
2022-10-23 18:51:36 +0500

    Add tab components

---
+++ project/src/components/tabs/tabs.tsx
@@ -0,0 +1,34 @@
+import { PropsWithChildren } from 'react';
+import { TabsContext } from './tabs-context';
+
+type TabsProps = PropsWithChildren<{
+  /**
+   * Active tab identifier
+   */
+  value: string;
+  onValueChange: (value: string) => void;
+  className: string;
+  activeTabClassName: string;
+}>
+
+export default function Tabs({children, value, onValueChange, className, activeTabClassName}: TabsProps) {
+  const switchTab = (id: string) => {
+    if (id !== value && onValueChange) {
+      onValueChange(id);
+    }
+  };
+
+  return (
+    <TabsContext.Provider
+      value={{
+        activeTab: value,
+        switchTab,
+        activeTabClassName
+      }}
+    >
+      <ul className={className}>
+        {children}
+      </ul>
+    </TabsContext.Provider>
+  );
+}
