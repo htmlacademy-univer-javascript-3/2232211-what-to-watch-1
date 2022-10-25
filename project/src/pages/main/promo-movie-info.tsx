6fd7c21c51fb475681772d1e2b8fb97b4624d12e
d.stukov <d.stukov@skbkontur.ru>
2022-10-25 10:31:54 +0500

    add MoviesList and PromoMovieInfo

---
+++ project/src/pages/main/promo-movie-info.tsx
@@ -0,0 +1,47 @@
+import type { Movie } from '../../types/movie';
+import MovieButton from '../../components/buttons/movie-button';
+import PlayIcon from '../../components/icons/play-icon';
+import AddIcon from '../../components/icons/add-icon';
+
+interface SelectedMovieInfoProps {
+  promoMovie: Movie;
+  movies: Movie[];
+}
+
+export default function PromoMovieInfo({
+  promoMovie,
+  movies
+}: SelectedMovieInfoProps) {
+  return (
+    <>
+      <div className='film-card__bg'>
+        <img src={promoMovie.backgroundImage} alt={promoMovie.name}/>
+      </div>
+
+      <div className='film-card__wrap'>
+        <div className='film-card__info'>
+          <div className='film-card__poster'>
+            <img src={promoMovie.posterImage} alt={`${promoMovie.name} poster`} width='218' height='327'/>
+          </div>
+
+          <div className='film-card__desc'>
+            <h2 className='film-card__title'>{promoMovie.name}</h2>
+            <p className='film-card__meta'>
+              <span className='film-card__genre'>{promoMovie.genre}</span>
+              <span className='film-card__year'>{promoMovie.released}</span>
+            </p>
+
+            <div className='film-card__buttons'>
+              <MovieButton icon={<PlayIcon/>}>
+                Play
+              </MovieButton>
+              <MovieButton icon={<AddIcon/>} moviesListCount={movies.filter((m) => m.isFavorite).length}>
+                My list
+              </MovieButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
