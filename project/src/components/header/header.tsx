fa3a380f7f245d3d1e836aed69d6ed556edf04db
d.stukov <d.stukov@skbkontur.ru>
2022-11-28 19:55:41 +0500

    use authorization in header with user

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -2,6 +2,9 @@ import Logo from '../logo/logo';
 import UserAvatar from '../user/user-avatar';
 import SignOut from '../sign-out/sign-out';
 import { ReactNode } from 'react';
+import { useAppSelector } from '../../hooks/store-helpers';
+import { AuthorizationStatus } from '../../constants';
+import SignIn from '../sign-in/sign-in';
 
 interface HeaderProps {
   userAvatarImageSource: string;
@@ -9,18 +12,44 @@ interface HeaderProps {
   navigation?: ReactNode;
 }
 
+interface UserLinkProps {
+  authorizationStatus: AuthorizationStatus;
+  userAvatarImageSource: string;
+}
+
+function UserBlockItems({authorizationStatus, userAvatarImageSource}: UserLinkProps) {
+  if (authorizationStatus !== AuthorizationStatus.Auth) {
+    return (
+      <li className='user-block__item'>
+        <SignIn/>
+      </li>
+    );
+  }
+
+  return (
+    <>
+      <li className='user-block__item'>
+        <UserAvatar imageSource={userAvatarImageSource} />
+      </li>
+      <li className='user-block__item'>
+        <SignOut />
+      </li>
+    </>
+  );
+}
+
 export default function Header({userAvatarImageSource, logoHref, navigation}: HeaderProps) {
+  const { authorizationStatus } = useAppSelector((state) => state.authorization);
+
   return (
     <header className='page-header film-card__head'>
       <Logo href={logoHref} />
       {navigation}
       <ul className='user-block'>
-        <li className='user-block__item'>
-          <UserAvatar imageSource={userAvatarImageSource} />
-        </li>
-        <li className='user-block__item'>
-          <SignOut />
-        </li>
+        <UserBlockItems
+          authorizationStatus={authorizationStatus}
+          userAvatarImageSource={userAvatarImageSource}
+        />
       </ul>
     </header>
   );
