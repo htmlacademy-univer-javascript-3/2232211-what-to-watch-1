a6ed8447d9c48fc0c5d5d5607c201f0fd4cad97d
d.stukov <d.stukov@skbkontur.ru>
2023-01-17 22:16:43 +0500

    toast autoClose: 5000ms -> 3000ms

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -45,7 +45,7 @@ function App(): JSX.Element {
       <AppRoutes />
       <ToastContainer
         position='top-center'
-        autoClose={5000}
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
