9209f14147a229e09e0713a347658b742d709519
d.stukov <d.stukov@skbkontur.ru>
2023-01-17 12:57:29 +0500

    Navigate to signin page after clicking on the MyList button if user is not authorized

--- project/src/components/buttons/my-list-button.tsx
+++ project/src/components/buttons/my-list-button.tsx
@@ -5,6 +5,9 @@ import { Movie } from '../../types/movie';
 import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/store-helpers';
 import { setFavoriteMovieAction } from '../../store/slices/favorite-movies-slice';
+import { getToken } from '../../services/token';
+import { useNavigate } from 'react-router-dom';
+import { PageLink } from '../../utils/links';
 
 interface MyListButtonProps {
   movie: Movie;
@@ -14,9 +17,16 @@ interface MyListButtonProps {
 export default function MyListButton({movie, updateMovieWithoutLoadingHandler}: MyListButtonProps) {
   const { favoriteMovies, favoriteMoviesLoading } = useAppSelector((state) => state.favoriteMovies);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const [myListButtonClick, setMyListButtonClick] = useState(false);
 
   const handleMyListClick = async () => {
+    if (!getToken()) {
+      // in case user is not authorized
+      navigate(PageLink.SignIn);
+      return;
+    }
+
     setMyListButtonClick(true);
     await dispatch(setFavoriteMovieAction(({
       movieId: movie.id,
